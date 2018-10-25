58b6b8e11cca18b325c4051d522da8f0e9264872
Slava Sobolev <sobolevslava@gmail.com>
2018-10-25 19:16:42 +0800

    whitelist summary

--- src/components/Create/Wizard/Summary/Whitelist.js
+++ src/components/Create/Wizard/Summary/Whitelist.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import styles from './Summary.module.styl';
+import styles from './Whitelist.module.styl';
 
 export default class Whitelist extends Component {
+  static propTypes = {
+    filters: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        param: PropTypes.string,
+        op: PropTypes.string,
+        value: PropTypes.string,
+      })
+    ),
+  };
+
+  static defaultProps = {
+    filters: [
+      { id: 0, param: 'Lives', op: 'in', value: 'London' },
+      { id: 1, param: 'Age', op: 'greater than', value: '35' },
+      { id: 2, param: 'Studies', op: 'in', value: 'SF' },
+      { id: 3, param: 'Gender', op: 'is', value: 'Female' },
+    ],
+  };
+
   render() {
-    return <div className={styles.container} />;
+    const { filters } = this.props;
+    return (
+      <div className={styles.container}>
+        {filters.map(condition => (
+          <div key={condition.id} className={styles.condition}>
+            <span className={styles.param}>{condition.param}</span>
+            <span className={styles.op}>{condition.op}</span>
+            <span className={styles.value}>{condition.value}</span>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
