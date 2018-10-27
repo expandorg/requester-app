e101b6e106ede6625317c54f506169fec2124e70
Slava Sobolev <sobolevslava@gmail.com>
2018-10-27 16:37:05 +0200

    settings dialog

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -4,14 +4,93 @@ import Content from '../shared/Content';
 import Navbar from '../shared/Navbar';
 import Sidebar from '../shared/Sidebar';
 
-// import styles from './Settings.module.styl';
+import Button from '../common/Button';
+import Input from '../common/Input';
+
+import Hero from '../Create/Wizard/Form/Hero';
+import DepositDialog from '../shared/Deposit/DepositDialog';
+
+import styles from './Settings.module.styl';
 
 export default class Settings extends Component {
+  state = {
+    email: '',
+    address: '',
+    password: '',
+
+    deposit: false,
+  };
+
+  handleWithdrawClick = () => {};
+
+  handleToggleDeposit = () => {
+    this.setState(({ deposit }) => ({ deposit: !deposit }));
+  };
+
+  handleInputChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
   render() {
+    const { email, address, password, deposit } = this.state;
     return (
       <Content title="Settings">
         <Navbar title="Settings" />
         <Sidebar />
+        <div className={styles.container}>
+          <div className={styles.form}>
+            <Hero value={200} title="Gems available" className={styles.hero} />
+            <div className={styles.actions}>
+              <Button
+                className={styles.moneyBtn}
+                onClick={this.handleToggleDeposit}
+              >
+                Deposit
+              </Button>
+              <Button
+                theme="secondary"
+                className={styles.moneyBtn}
+                onClick={this.handleWithdrawClick}
+              >
+                Withdraw
+              </Button>
+            </div>
+          </div>
+          <div className={styles.form}>
+            <div className={styles.title}>Account Details</div>
+            <div className={styles.field}>
+              <Input
+                value={address}
+                className={styles.input}
+                placeholder="Account address"
+                name="address"
+                onChange={this.handleInputChange}
+              />
+              <button className={styles.updateBtn}>Update</button>
+            </div>
+            <div className={styles.field}>
+              <Input
+                value={email}
+                className={styles.input}
+                placeholder="Email address"
+                name="email"
+                onChange={this.handleInputChange}
+              />
+              <button className={styles.updateBtn}>Update</button>
+            </div>
+            <div className={styles.field}>
+              <Input
+                value={password}
+                className={styles.input}
+                placeholder="New password"
+                name="password"
+                onChange={this.handleInputChange}
+              />
+              <button className={styles.updateBtn}>Update</button>
+            </div>
+          </div>
+        </div>
+        {deposit && <DepositDialog onHide={this.handleToggleDeposit} />}
       </Content>
     );
   }
