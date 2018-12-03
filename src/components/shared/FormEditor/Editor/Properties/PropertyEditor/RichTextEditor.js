25cc0346ebeff1c105665a04b63a2eeffc1c92e9
Slava Sobolev <sobolevslava@gmail.com>
2018-12-03 23:14:41 +0300

    update api clinet

--- src/components/shared/FormEditor/Editor/Properties/PropertyEditor/RichTextEditor.js
+++ src/components/shared/FormEditor/Editor/Properties/PropertyEditor/RichTextEditor.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-import Input from '../../../../../common/Input';
+import { Editor, EditorState } from 'draft-js';
 
 import styles from './RichTextEditor.module.styl';
 
 export default class RichTextEditor extends Component {
   static propTypes = {
-    value: PropTypes.string,
-    placeholder: PropTypes.string,
-    onChange: PropTypes.func.isRequired,
+    // value: PropTypes.string,
+    // placeholder: PropTypes.string,
+    // onChange: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
-    value: undefined,
-    placeholder: undefined,
+    //   value: undefined,
+    //   placeholder: undefined,
   };
 
-  handleChange = ({ target }) => {
-    const { onChange } = this.props;
-    onChange(target.value);
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = { editorState: EditorState.createEmpty() };
+  }
+
+  handleChange = editorState => this.setState({ editorState });
 
   render() {
-    const { value, placeholder } = this.props;
-
+    // const { value, placeholder } = this.props;
     return (
-      <Input
+      <Editor
         className={styles.input}
-        value={value}
-        placeholder={placeholder}
+        editorState={this.state.editorState}
         onChange={this.handleChange}
       />
     );
