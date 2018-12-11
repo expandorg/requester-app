0bbcbf17d63043f427beb9df5c18bab3cabf2963
Slava Sobolev <sobolevslava@gmail.com>
2018-12-11 22:31:03 +0300

    data summary empty state

--- src/components/Draft/Wizard/Summary/Data/Data.js
+++ src/components/Draft/Wizard/Summary/Data/Data.js
@@ -9,6 +9,8 @@ import Table from './Table';
 import { draftProps } from '../../../../shared/propTypes';
 import { fetch } from '../../../../../sagas/dataSagas';
 
+import { hasData } from '../../../wizard';
+
 import styles from './Data.module.styl';
 
 const mapDispatchToProps = dispatch => bindActionCreators({ fetch }, dispatch);
@@ -26,6 +28,7 @@ class Data extends Component {
   componentDidMount() {
     const { draft } = this.props;
     const { page } = this.state;
+
     if (draft.dataId) {
       this.props.fetch(draft.id, draft.dataId, page);
     }
@@ -40,9 +43,18 @@ class Data extends Component {
   render() {
     const { draft } = this.props;
     const { page } = this.state;
+
+    const data = hasData(draft);
     return (
       <div className={styles.container}>
-        <Table draft={draft} page={page} onChangePage={this.handleChangePage} />
+        {data && (
+          <Table
+            draft={draft}
+            page={page}
+            onChangePage={this.handleChangePage}
+          />
+        )}
+        {!data && <div className={styles.emptyData}>No data uploaded.</div>}
       </div>
     );
   }
