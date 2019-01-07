21077130dc26301c93194b9382c16c7748d75c40
Slava Sobolev <sobolevslava@gmail.com>
2019-01-07 18:57:47 +0300

    unify deposit/withdraw conditions

--- src/components/shared/Deposit/Deposit.js
+++ src/components/shared/Deposit/Deposit.js
@@ -4,23 +4,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { RequestStates } from '@gemsorg/app-utils';
 import { userProps } from '@gemsorg/app-auth';
 
-import {
-  fetchBalanceStateSelector,
-  transactionStateSelector,
-} from '@gemsorg/app-gemtokens/selectors';
-
+import { fetchBalanceStateSelector } from '@gemsorg/app-gemtokens/selectors';
 import { fetchGemsBalance } from '@gemsorg/app-gemtokens/sagas';
 
 import DepositDialog from './DepositDialog';
-import AddressDialog from '../Address/AddressDialog';
-import ConfirmEmailDialog from '../Email/ConfirmEmailDialog';
+import AccountConfirmed from '../AccountConfirmed';
 
 const mapStateToProps = state => ({
   fetchState: fetchBalanceStateSelector(state),
-  txState: transactionStateSelector(state),
 });
 
 const mapDispatchToProps = dispatch =>
@@ -29,14 +22,7 @@ const mapDispatchToProps = dispatch =>
 class Deposit extends Component {
   static propTypes = {
     user: userProps.isRequired,
-    txState: PropTypes.string.isRequired,
     fetchGemsBalance: PropTypes.func.isRequired,
-  };
-
-  state = {
-    dialog: false,
-    confirm: false,
-    address: false,
   };
 
   componentDidMount() {
@@ -53,58 +39,18 @@ class Deposit extends Component {
     }
   }
 
-  handleToggle = () => {
-    const { user, txState } = this.props;
-    if (txState === RequestStates.Fetching) {
-      return;
-    }
-
-    const { dialog } = this.state;
-    try {
-      if (!dialog) {
-        if (!user.emailConfirmed) {
-          throw new Error('confirm');
-        }
-        if (!user.address) {
-          throw new Error('address');
-        }
-      }
-      this.setState({ dialog: !dialog });
-    } catch (e) {
-      this.setState({ [e.message]: true });
-    }
-  };
-
-  handleHideAddress = () => {
-    const { user } = this.props;
-    this.setState({ address: false });
-    if (user.address) {
-      this.handleToggle();
-    }
-  };
-
-  handleHideConfirm = () => {
-    const { user } = this.props;
-    this.setState({ confirm: false });
-    if (user.emailConfirmed) {
-      this.handleToggle();
-    }
-  };
-
   render() {
     const { user, children } = this.props;
-    const { dialog, address, confirm } = this.state;
+
     return (
-      <>
-        {children({ onToggleDepsoit: this.handleToggle })}
-        {confirm && (
-          <ConfirmEmailDialog user={user} onHide={this.handleHideConfirm} />
+      <AccountConfirmed user={user}>
+        {({ onToggle, dialog }) => (
+          <>
+            {children({ onToggleDepsoit: onToggle })}
+            {dialog && <DepositDialog user={user} onHide={onToggle} />}
+          </>
         )}
-        {address && (
-          <AddressDialog user={user} onHide={this.handleHideAddress} />
-        )}
-        {dialog && <DepositDialog user={user} onHide={this.handleToggle} />}
-      </>
+      </AccountConfirmed>
     );
   }
 }
--- src/components/shared/Withdraw/Withdraw.js
+++ src/components/shared/Withdraw/Withdraw.js
@@ -1,87 +1,26 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-import { connect } from 'react-redux';
-
-import { RequestStates } from '@gemsorg/app-utils';
 import { userProps } from '@gemsorg/app-auth';
 
-import { transactionStateSelector } from '@gemsorg/app-gemtokens/selectors';
-
 import WithdrawDialog from './WithdrawDialog';
-import AddressDialog from '../Address/AddressDialog';
-import ConfirmEmailDialog from '../Email/ConfirmEmailDialog';
+import AccountConfirmed from '../AccountConfirmed';
 
-const mapStateToProps = state => ({
-  txState: transactionStateSelector(state),
-});
-
-class Withdraw extends Component {
+export default class Withdraw extends Component {
   static propTypes = {
     user: userProps.isRequired,
-    txState: PropTypes.string.isRequired,
-  };
-
-  state = {
-    dialog: false,
-    confirm: false,
-    address: false,
-  };
-
-  handleToggle = () => {
-    const { user, txState } = this.props;
-    if (txState === RequestStates.Fetching) {
-      return;
-    }
-
-    const { dialog } = this.state;
-    try {
-      if (!dialog) {
-        if (!user.emailConfirmed) {
-          throw new Error('confirm');
-        }
-        if (!user.address) {
-          throw new Error('address');
-        }
-      }
-      this.setState({ dialog: !dialog });
-    } catch (e) {
-      this.setState({ [e.message]: true });
-    }
-  };
-
-  handleHideAddress = () => {
-    const { user } = this.props;
-    this.setState({ address: false });
-    if (user.address) {
-      this.handleToggle();
-    }
-  };
-
-  handleHideConfirm = () => {
-    const { user } = this.props;
-    this.setState({ confirm: false });
-    if (user.emailConfirmed) {
-      this.handleToggle();
-    }
   };
 
   render() {
     const { user, children } = this.props;
-    const { dialog, address, confirm } = this.state;
     return (
-      <>
-        {children({ onToggleWithdraw: this.handleToggle })}
-        {confirm && (
-          <ConfirmEmailDialog user={user} onHide={this.handleHideConfirm} />
+      <AccountConfirmed user={user}>
+        {({ onToggle, dialog }) => (
+          <>
+            {children({ onToggleWithdraw: onToggle })}
+            {dialog && <WithdrawDialog user={user} onHide={onToggle} />}
+          </>
         )}
-        {address && (
-          <AddressDialog user={user} onHide={this.handleHideAddress} />
-        )}
-        {dialog && <WithdrawDialog user={user} onHide={this.handleToggle} />}
-      </>
+      </AccountConfirmed>
     );
   }
 }
-
-export default connect(mapStateToProps)(Withdraw);
