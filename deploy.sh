eec93f966e268cbf059ceaf16e3a8492f4d658f0
Kieran O'Reilly <kieran@gems.org>
2019-01-09 14:57:46 -0800

    Update deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+case $1 in
+  production)
+    subdomain=requester;;
+  ropsten)
+    subdomain=dev-requester;;
+esac
+
+url=gs://${subdomain}.expand.org
+old_hash=$(gsutil ls $url | sed -nE "s,${url}/app-(.+)\.js$,\1,p")
+
+rm -rf ./public/static ./public/portal-* ./public/index.html
+npm install
+NODE_ENV=$1 webpack
+
+gsutil cp -r ./public/static ./public/portal-* ./public/index.html ${url}/
+gsutil rm ${url}/portal-${old_hash}*
