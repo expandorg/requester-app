b48b20e2ef36dabfc9ee6a56e44210e706d7591f
Slava Sobolev <sobolevslava@gmail.com>
2019-03-05 16:41:54 +0800

    logic + selection

--- src/components/shared/FormEditor/Logic/LogicEditor.js
+++ src/components/shared/FormEditor/Logic/LogicEditor.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import { Button } from '@expandorg/components';
 import { moduleProps } from '@expandorg/modules';
 
 import styles from './LogicEditor.module.styl';
 
-export default function LogicEditor({ module }) {
+export default function LogicEditor({ module, onCancel }) {
   console.log(module);
+
+  const handleSave = () => {};
 
   return (
     <div className={styles.container}>
       <h1>test</h1>
+      <div className={styles.actions}>
+        <Button theme="grey" size="small" onClick={onCancel}>
+          Cancel
+        </Button>
+        <Button onClick={handleSave} size="small">
+          Save
+        </Button>
+      </div>
     </div>
   );
 }
 
 LogicEditor.propTypes = {
   module: moduleProps.isRequired,
+  onCancel: PropTypes.func.isRequired,
 };
