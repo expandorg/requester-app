d75ce9dd822c91482689f4a6464e7cbc6eddaafb
Slava Sobolev <sobolevslava@gmail.com>
2019-03-05 12:46:18 +0800

    refactoring

--- src/components/shared/FormEditor/Canvas/Spacer.js
+++ src/components/shared/FormEditor/Canvas/Spacer.js
@@ -4,19 +4,23 @@ import cn from 'classnames';
 
 import styles from './Spacer.module.styl';
 
-export default function Spacer({ className, visible }) {
-  return (
-    <div
-      className={cn(styles.spacer, { [styles.expanded]: visible }, className)}
-    />
-  );
+export default function Spacer({ className, visible, width }) {
+  const style = visible
+    ? {
+        width,
+        minWidth: width,
+      }
+    : undefined;
+  return <div className={cn(styles.spacer, className)} style={style} />;
 }
 
 Spacer.propTypes = {
   visible: PropTypes.bool.isRequired,
+  width: PropTypes.number,
   className: PropTypes.string,
 };
 
 Spacer.defaultProps = {
   className: null,
+  width: 540,
 };
