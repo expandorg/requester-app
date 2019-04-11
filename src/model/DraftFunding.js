0faca09b544caa7d4b7dab0abc87681f3454df03
Slava Sobolev <sobolevslava@gmail.com>
2019-04-11 14:59:10 +0800

    payemnt fix

--- src/model/DraftFunding.js
+++ src/model/DraftFunding.js
@@ -1,15 +1,15 @@
 // @flow
 
 import { validateForm, rules } from '@expandorg/validation';
-import { ge } from './validation';
+import { gt } from './validation';
 
 import { type Draft } from './types.flow';
 import { DraftStatus } from './enums';
 
 export default class DraftFunding {
   static rules = {
-    balance: [[rules.isNumber, 'Should be a positive number'], ge(0)],
-    reward: [[rules.isNumber, 'Should be a positive number'], ge(0)],
+    balance: [[rules.isNumber, 'Should be a positive number'], gt(0)],
+    reward: [[rules.isNumber, 'Should be a positive number'], gt(0)],
   };
 
   static getFundingRequestParams(
--- src/model/validation.js
+++ src/model/validation.js
@@ -2,6 +2,10 @@
 
 export const ge = (g: number = 0) => [
   (v: number) => v >= g,
+  `Should be equal or greater then ${g}`,
+];
+export const gt = (g: number = 0) => [
+  (v: number) => v > g,
   `Should be greater then ${g}`,
 ];
 
