ad46a058ee4d552338c496ae2202c1a5352335a4
Slava Sobolev <sobolevslava@gmail.com>
2019-05-15 18:30:23 +0300

    preview fix

--- src/components/shared/FormEditor/Canvas/Toolbar/PreviewCtx.js
+++ src/components/shared/FormEditor/Canvas/Toolbar/PreviewCtx.js
@@ -1,51 +1,63 @@
-import { useRef, useEffect } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { moduleProps } from '@expandorg/modules';
 import PreviewFormTab from '../../../../../common/popups/PreviewFormTab';
 
 const updatePreview = (tab, modules, variables) => {
-  if (tab.current) {
-    const wnd = tab.current.getWindow();
+  if (tab) {
+    const wnd = tab.getWindow();
     if (wnd) {
       wnd.postMessage({ type: 'updateForm', form: { modules }, variables });
     }
   }
 };
 
-export default function PreviewCtx({ children, modules, variables }) {
-  const tab = useRef();
-  const handleMessage = ({ data }) => {
+export default class PreviewCtx extends Component {
+  static propTypes = {
+    modules: PropTypes.arrayOf(moduleProps),
+    variables: PropTypes.object, // eslint-disable-line
+  };
+
+  static defaultProps = {
+    modules: [],
+    variables: null,
+  };
+
+  tab = null;
+
+  componentDidMount() {
+    window.addEventListener('message', this.handleMessage, false);
+  }
+
+  componentDidUpdate({ modules: prev }) {
+    const { modules, variables } = this.props;
+    if (prev !== modules) {
+      updatePreview(this.tab, modules, variables);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('message', this.handleMessage);
+  }
+
+  handleMessage = ({ data }) => {
+    const { modules, variables } = this.props;
     if (typeof data === 'object' && data.type === 'previewReady') {
-      updatePreview(tab, modules, variables);
+      updatePreview(this.tab, modules, variables);
     }
   };
 
-  useEffect(() => {
-    window.addEventListener('message', handleMessage, false);
-    return () => window.removeEventListener('message', handleMessage);
-  }, []);
+  render() {
+    const { children } = this.props;
 
-  useEffect(() => {
-    updatePreview(tab, modules, variables);
-  }, [modules]);
-
-  const handlePreview = () => {
-    if (!tab.current) {
-      tab.current = new PreviewFormTab();
-    }
-    tab.current.open();
-  };
-
-  return children({ onPreview: handlePreview });
+    return children({
+      onPreview: () => {
+        if (!this.tab) {
+          this.tab = new PreviewFormTab();
+        }
+        this.tab.open();
+      },
+    });
+  }
 }
-
-PreviewCtx.propTypes = {
-  modules: PropTypes.arrayOf(moduleProps),
-  variables: PropTypes.object, // eslint-disable-line
-};
-
-PreviewCtx.defaultProps = {
-  modules: [],
-  variables: null,
-};
