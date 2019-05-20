d9332dd7bfaa2cdfb383cf406b2e21ba16c085cc
Slava Sobolev <sobolevslava@gmail.com>
2019-05-20 21:37:19 +0300

    image region fix

--- src/components/shared/FormEditor/Properties/PropertyEditor/ImageRegionEditor.js
+++ src/components/shared/FormEditor/Properties/PropertyEditor/ImageRegionEditor.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 import LinkedValueEditor from './LinkedValueEditor';
 
+const stringifyVal = v =>
+  Reflect.ownKeys(v)
+    .map(c => `${c}: ${v[c]}`)
+    .join(', ');
+
 const stringify = v => {
   if (typeof v === 'string') {
     return v;
   }
-  return v
-    ? Reflect.ownKeys(v)
-        .map(c => `${c}: ${v[c]}`)
-        .join(', ')
-    : undefined;
+  if (Array.isArray(v)) {
+    return v.map(val => stringifyVal(val)).join('\r\n\r\n');
+  }
+  return v ? stringifyVal(v) : undefined;
 };
 
 export default function ImageRegionEditor(props) {
