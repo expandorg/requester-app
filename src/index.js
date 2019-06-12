f755c474637a66453027f5e9dae421c458eee727
Slava Sobolev <sobolevslava@gmail.com>
2019-06-12 12:51:32 +0300

    add inspectlet ,resolves #106

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { setAppElement } from '@expandorg/components';
 
+import settings from './common/settings';
+import inspectlet from './common/inspectlet';
+
 import App from './App';
 
 const appElement = document.getElementById('root');
 
 setAppElement(appElement);
+inspectlet(settings.inspectletId);
 
 render(<App />, appElement);
