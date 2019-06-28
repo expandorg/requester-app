f33ea401b3b8ba3fa56dbd5bd2ae9045ee16ad89
Slava Sobolev <sobolevslava@gmail.com>
2019-06-28 18:01:42 +0300

    menu sceoll

--- src/components/Draft/Forms/Steps/controls.js
+++ src/components/Draft/Forms/Steps/controls.js
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState, useCallback, useRef } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
+import { Portal } from '@expandorg/components';
 import { ReactComponent as SettingsIcon } from './settings.svg';
 import { ReactComponent as MenuIcon } from './menu.svg';
 
@@ -43,12 +44,35 @@ export function NavItemText({ children }) {
 }
 
 export function NavItemContextMenu({ visible, onToggle, children }) {
+  const btn = useRef(null);
+  const [pos, setPos] = useState(null);
+
+  const toggle = useCallback(
+    evt => {
+      evt.preventDefault();
+      evt.stopPropagation();
+      if (!visible) {
+        setPos(btn.current.getBoundingClientRect());
+      }
+      onToggle();
+    },
+    [onToggle, visible]
+  );
   return (
     <>
-      <button className={styles.contextMenuButton} onClick={onToggle}>
+      <button ref={btn} className={styles.contextMenuButton} onClick={toggle}>
         <MenuIcon />
       </button>
-      {visible && <ContextMenu onHide={onToggle}>{children}</ContextMenu>}
+      {visible && pos && (
+        <Portal
+          className={styles.portal}
+          style={{ left: pos.left, top: pos.top }}
+        >
+          <ContextMenu className={styles.contextMenu} onHide={onToggle}>
+            {children}
+          </ContextMenu>
+        </Portal>
+      )}
     </>
   );
 }
