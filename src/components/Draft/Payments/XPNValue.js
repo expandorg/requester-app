b74de1967d0ac7049eb58ce9a757a9afc1233f4d
Slava Sobolev <sobolevslava@gmail.com>
2019-07-11 17:46:08 +0300

    comment api

--- src/components/Draft/Payments/XPNValue.js
+++ src/components/Draft/Payments/XPNValue.js
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
-import { priceApi } from '../../../api/XPNPriceApi';
+import React from 'react';
+// import { priceApi } from '../../../api/XPNPriceApi';
 
 import styles from './XPNValue.module.styl';
 
-let tokenPrice = 0.002;
+const tokenPrice = 0.002;
 
-async function fetchPrice() {
-  try {
-    if (tokenPrice !== null) {
-      const response = await priceApi.fetchPrice();
-      tokenPrice = response.price;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function fetchPrice() {
+//   try {
+//     if (tokenPrice !== null) {
+//       const response = await priceApi.fetchPrice();
+//       tokenPrice = response.price;
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 export default function XPNValue() {
-  useEffect(() => fetchPrice(), []);
+  // useEffect(() => fetchPrice(), []);
 
   if (tokenPrice === null) {
     return null;
