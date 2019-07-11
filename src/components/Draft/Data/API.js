60740ff890b802f3844b6beeecd82ce638be8f5f
Slava Sobolev <sobolevslava@gmail.com>
2019-07-11 23:14:05 +0300

    data upload

--- src/components/Draft/Data/API.js
+++ src/components/Draft/Data/API.js
@@ -1,3 +1,17 @@
+import React from 'react';
+
+import styles from './API.module.styl';
+
 export default function API() {
-  return null;
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        We’re getting the ready for you soon so hold tight. If you have any
+        questions don’t hesitate to contact us.
+      </div>
+      <a href="mailto:hello@expand.org" className={styles.link}>
+        contact us
+      </a>
+    </div>
+  );
 }
