4543dee941fef8d29027129419dd0d14a3176279
Slava Sobolev <sobolevslava@gmail.com>
2019-07-24 20:41:56 +0300

    uniq names

--- src/components/shared/FormEditor/modules.js
+++ src/components/shared/FormEditor/modules.js
@@ -1,5 +1,6 @@
 // @flow
 import generate from 'nanoid/generate';
+import { getFormModulesNames } from '@expandorg/modules/model';
 import type {
   ModuleControlMeta,
   Module,
@@ -8,16 +9,29 @@ import type {
 export const supportNesting = ({ editor }: ModuleControlMeta): boolean =>
   !!(editor && editor.properties && editor.properties.modules);
 
-export const newModuleId = ({ type }: ModuleControlMeta) =>
-  `${type}-${generate('1234567890abcdef', 4)}`;
+export const newModuleId = (modules: Array<Module>) => {
+  const names = new Set(getFormModulesNames({ modules }));
+  return ({ type }: ModuleControlMeta) => {
+    let i = 0;
+    while (i < 1000) {
+      const name = `${type}${i}`;
+      if (!names.has(name)) {
+        return name;
+      }
+      i += 1;
+    }
+    return `${type}-${generate('1234567890abcdef', 3)}`;
+  };
+};
 
 export const createModule = (
   meta: ModuleControlMeta,
+  modules: Array<Module>,
   isDragging: boolean = false
 ): Module => ({
   ...(meta.editor && meta.editor.defaults),
   type: meta.type,
-  name: newModuleId(meta),
+  name: newModuleId(modules)(meta),
   isDragging,
   modules: supportNesting(meta) ? [] : undefined,
 });
