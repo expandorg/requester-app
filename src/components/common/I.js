98e9fca7e92ab18939a19e8e8f8e76d40344da22
Slava Sobolev <sobolevslava@gmail.com>
2019-08-06 18:07:44 +0300

    update i icon

--- src/components/common/I.js
+++ src/components/common/I.js
@@ -4,13 +4,13 @@ import cn from 'classnames';
 
 import { Tooltip } from '@expandorg/components';
 
-import { ReactComponent as InfoMark } from '@expandorg/uikit/assets/i.svg';
+import { ReactComponent as InfoMark } from '../../assets/circle-i.svg';
 
 import styles from './I.module.styl';
 
 const I = Tooltip(({ className, children, ...rest }) => (
   <span className={cn(styles.i, className)} {...rest}>
-    <InfoMark />
+    <InfoMark width="18" height="18" viewBox="0 0 20 20" />
     {children}
   </span>
 ));
