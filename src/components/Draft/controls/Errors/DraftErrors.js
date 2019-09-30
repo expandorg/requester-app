682a6d51ec9cae937f67b2125b6d45f7184bcbe3
Slava Sobolev <sobolevslava@gmail.com>
2019-09-30 16:56:31 +0300

    warning icon redesign

--- src/components/Draft/controls/Errors/DraftErrors.js
+++ src/components/Draft/controls/Errors/DraftErrors.js
@@ -1,8 +1,6 @@
 import React, { useRef, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
-import { ReactComponent as Down } from '../../../../assets/arrow_drop_down.svg';
-import { ReactComponent as Up } from '../../../../assets/arrow_drop_up.svg';
 import { ReactComponent as Warning } from '../../../../assets/warning.svg';
 
 import DraftValidator from '../../../../model/DraftValidator';
@@ -43,28 +41,27 @@ export default function DraftErrors({ validation, onNavigate }) {
     return null;
   }
 
-  const UpDown = visible ? Up : Down;
   return (
-    <div className={styles.container} ref={ref}>
-      <button className={styles.button} onClick={toggle}>
-        <Warning className={styles.warning} />
-        {count} {count === 1 ? 'Error' : 'Errors'}
-        <UpDown className={styles.updown} />
-      </button>
-      {visible && (
-        <ErrorsContextMenu pos={pos} validation={validation} onHide={toggle}>
-          {DraftErrorsBuilder.errorMessages(validation).map(
-            ({ path, message, nav }) => (
-              <ErrorsMenuItem
-                key={path}
-                path={path}
-                message={message}
-                onClick={() => navigate(nav)}
-              />
-            )
-          )}
-        </ErrorsContextMenu>
-      )}
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        <button className={styles.button} onClick={toggle} ref={ref}>
+          <Warning className={styles.warning} />
+        </button>
+        {visible && (
+          <ErrorsContextMenu pos={pos} validation={validation} onHide={toggle}>
+            {DraftErrorsBuilder.errorMessages(validation).map(
+              ({ path, message, nav }) => (
+                <ErrorsMenuItem
+                  key={path}
+                  path={path}
+                  message={message}
+                  onClick={() => navigate(nav)}
+                />
+              )
+            )}
+          </ErrorsContextMenu>
+        )}
+      </div>
     </div>
   );
 }
