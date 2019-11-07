85616e9efc7b4dc6cc7f77488035909ce87d5f57
Slava Sobolev <sobolevslava@gmail.com>
2019-11-07 14:46:42 +0100

    api bg

--- src/components/Draft/Data/API.js
+++ src/components/Draft/Data/API.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import cn from 'classnames';
 
 import styles from './API.module.styl';
 
 export default function API() {
   return (
     <div className={styles.container}>
+      <div className={styles.img} />
       <div className={styles.text}>
         We’re getting the ready for you soon so hold tight. If you have any
         questions don’t hesitate to contact us.
       </div>
-      <a href="mailto:hello@expand.org" className={styles.link}>
+      <a
+        className={cn(
+          'gem-button',
+          'gem-button-small',
+          'gem-button-white-blue',
+          styles.link
+        )}
+        href="mailto:hello@expand.org"
+      >
         contact us
       </a>
     </div>
