5a47e6750c5af5ba08b7984299c711f2174da647
Slava Sobolev <sobolevslava@gmail.com>
2019-11-14 16:31:50 +0100

    prev score method compitablility

--- src/components/Preview/workflow/VerificationState.js
+++ src/components/Preview/workflow/VerificationState.js
@@ -1,13 +1,20 @@
 // @flow
 import { getVerificationResponse } from '@expandorg/modules/model';
+import { moduleControls } from '@expandorg/modules/app';
 
 import { type Draft } from '../../../model/types.flow';
 import { createRepeatState, type WorkflowState } from './defs';
 
 export default class VerificationState {
   static getNextState(draft: Draft, response: any): WorkflowState {
-    const score = getVerificationResponse(response, draft.verificationForm);
-    console.log('verification score:', score);
+    if (draft.verificationForm) {
+      const score = getVerificationResponse(
+        response,
+        draft.verificationForm,
+        moduleControls
+      );
+      console.log('verification score:', score);
+    }
     return createRepeatState();
   }
 }
